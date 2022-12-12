b6aaae80625101c87c458ea94edab586975aafd1
Elitex <jain.yashu2211@gmail.com>
2022-12-12 10:59:04 +0530

    Connect.js fixed

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('../config/config.json')
+const colors = require('colors')
 
+async function connect() {
+    mongoose.set('strictQuery', false)
+
+    mongoose.connect(config.MONGODB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    mongoose.connection.once("open", () => {
+        console.log(`[MONGO] Connected to Database`.green)
+    });
+    return;
+}
+
+module.exports = connect;
