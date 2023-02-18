2dfa0eca1b699368091b815e528d917aa3fb0dd0
Samuu_X <samueljuegos83@gmail.com>
2023-02-18 00:54:51 +0000

    📦 NEW: Nuevo Code
    Signed-off-by: Samuu_X <samueljuegos83@gmail.com>

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -4,8 +4,18 @@ const colors = require('colors');
 module.exports = {
     name: "ready"
 };
-
+const { ActivityType } = require('discord.js')
 client.once('ready', async () => {
+client.user.setPresence({
+    activities: [
+      {
+        name: `Modulos de sorteos`,
+        type: ActivityType.Listening,
+        // url: 'https://twitch.tv/Samuu_X'
+      }
+    ],
+    status: 'idle'
+  })
     console.log("----------------------------------------".white);
     console.log(`[READY] ${client.user.tag} is up and ready to go.`.bold)
     console.log("----------------------------------------".white);
